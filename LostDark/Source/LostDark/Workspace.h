5a9d25c26fbe447b00667ce536f9ad5207c475af
SungHoon <ksh920610@gmail.com>
2019-04-09 04:07:44 +0900

    팀원별 작업공간 폴더 생성및 연습공간 Map 생성.

---
+++ LostDark/Source/LostDark/Workspace.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class LOSTDARK_API Workspace
+{
+public:
+	Workspace();
+	~Workspace();
+};
