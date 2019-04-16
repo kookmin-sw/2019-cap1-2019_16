d9b447a3cb0eb6e09e9ab2c28db9e792e32b7cfa
SungHoon <ksh920610@gmail.com>
2019-04-17 03:27:09 +0900

    [master 수정] 백업본 넣고 최종 수정

---
+++ LostDark/Intermediate/Build/Win64/UE4Editor/Inc/LostDark/LostDarkCharacter.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LOSTDARK_LostDarkCharacter_generated_h
+#error "LostDarkCharacter.generated.h already included, missing '#pragma once' in LostDarkCharacter.h"
+#endif
+#define LOSTDARK_LostDarkCharacter_generated_h
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_RPC_WRAPPERS
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALostDarkCharacter(); \
+	friend struct Z_Construct_UClass_ALostDarkCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ALostDarkCharacter, ACharacter, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LostDark"), NO_API) \
+	DECLARE_SERIALIZER(ALostDarkCharacter)
+
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesALostDarkCharacter(); \
+	friend struct Z_Construct_UClass_ALostDarkCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ALostDarkCharacter, ACharacter, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/LostDark"), NO_API) \
+	DECLARE_SERIALIZER(ALostDarkCharacter)
+
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ALostDarkCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALostDarkCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALostDarkCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALostDarkCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALostDarkCharacter(ALostDarkCharacter&&); \
+	NO_API ALostDarkCharacter(const ALostDarkCharacter&); \
+public:
+
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ALostDarkCharacter(ALostDarkCharacter&&); \
+	NO_API ALostDarkCharacter(const ALostDarkCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ALostDarkCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALostDarkCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ALostDarkCharacter)
+
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	FORCEINLINE static uint32 __PPO__CameraBoom() { return STRUCT_OFFSET(ALostDarkCharacter, CameraBoom); } \
+	FORCEINLINE static uint32 __PPO__FollowCamera() { return STRUCT_OFFSET(ALostDarkCharacter, FollowCamera); }
+
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_9_PROLOG
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_RPC_WRAPPERS \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_INCLASS \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LostDark_Source_LostDark_LostDarkCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	LostDark_Source_LostDark_LostDarkCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LostDark_Source_LostDark_LostDarkCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
---
+++ LostDark/Intermediate/Build/Win64/UE4Editor/Inc/LostDark/LostDarkGameMode.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef LOSTDARK_LostDarkGameMode_generated_h
+#error "LostDarkGameMode.generated.h already included, missing '#pragma once' in LostDarkGameMode.h"
+#endif
+#define LOSTDARK_LostDarkGameMode_generated_h
+
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_RPC_WRAPPERS
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesALostDarkGameMode(); \
+	friend struct Z_Construct_UClass_ALostDarkGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ALostDarkGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/LostDark"), LOSTDARK_API) \
+	DECLARE_SERIALIZER(ALostDarkGameMode)
+
+
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesALostDarkGameMode(); \
+	friend struct Z_Construct_UClass_ALostDarkGameMode_Statics; \
+public: \
+	DECLARE_CLASS(ALostDarkGameMode, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/LostDark"), LOSTDARK_API) \
+	DECLARE_SERIALIZER(ALostDarkGameMode)
+
+
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	LOSTDARK_API ALostDarkGameMode(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ALostDarkGameMode) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(LOSTDARK_API, ALostDarkGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALostDarkGameMode); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	LOSTDARK_API ALostDarkGameMode(ALostDarkGameMode&&); \
+	LOSTDARK_API ALostDarkGameMode(const ALostDarkGameMode&); \
+public:
+
+
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	LOSTDARK_API ALostDarkGameMode(ALostDarkGameMode&&); \
+	LOSTDARK_API ALostDarkGameMode(const ALostDarkGameMode&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(LOSTDARK_API, ALostDarkGameMode); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ALostDarkGameMode); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ALostDarkGameMode)
+
+
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_PRIVATE_PROPERTY_OFFSET
+#define LostDark_Source_LostDark_LostDarkGameMode_h_9_PROLOG
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_RPC_WRAPPERS \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_INCLASS \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define LostDark_Source_LostDark_LostDarkGameMode_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_PRIVATE_PROPERTY_OFFSET \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_INCLASS_NO_PURE_DECLS \
+	LostDark_Source_LostDark_LostDarkGameMode_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID LostDark_Source_LostDark_LostDarkGameMode_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
