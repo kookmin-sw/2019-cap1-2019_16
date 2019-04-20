24b37af4d379e5a344ca0025dd05bd4e71591e1a
SungHoon <ksh920610@gmail.com>
2019-04-20 21:24:29 +0900

    [프로젝트 에러 수정] 컴파일 안되는 문제 해결 - saved, Intermediate 삭제하고 다시 리빌드 하니까 됨.

--- LostDark/Intermediate/Build/Win64/UE4Editor/Inc/LostDark/LostDark.init.gen.cpp
+++ LostDark/Intermediate/Build/Win64/UE4Editor/Inc/LostDark/LostDark.init.gen.cpp
@@ -19,8 +19,8 @@ void EmptyLinkFunctionForGeneratedCodeLostDark_init() {}
 			static const UE4CodeGen_Private::FPackageParams PackageParams = {
 				"/Script/LostDark",
 				PKG_CompiledIn | 0x00000000,
-				0x4AD911E6,
-				0xFAB31BFE,
+				0x61498EB1,
+				0x3316FC11,
 				nullptr, 0,
 				METADATA_PARAMS(nullptr, 0)
 			};
